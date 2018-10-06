f9fdfea3266f3f15ce7e5faa6efdc7a1de060a84
keitakn <keita.koga.work@gmail.com>
2018-10-07 02:06:04 +0900

    :art: #1 色々と方向転換をしたので構造を大幅に変更（詳細は下記）
    - ベースのOSとしてAlpine Linuxを利用するように変更
    - nginxの設定をなるべくシンプルな物に変更
    - コンテナのサービス名をなるべく分かりやすい形（nginx,php）に変更

--- data/html/index.php
+++ data/html/index.php
@@ -1,2 +1,11 @@
-<?php
-echo phpinfo();
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div>🐱🐱🐱ぬこ🐱🐱🐱ねこ🐱🐱🐱</div>
+<?php echo phpinfo(); ?>
+</body>
+</html>

295252a962cf4d6708b9447a257deb4bb01f1571
keitakn <keita.koga.work@gmail.com>
2018-10-12 02:43:39 +0900

    :wrench: #7 qiita-stockerのDBと必要なユーザーを作成するように変更

--- mysql/initial.sql
+++ mysql/initial.sql
@@ -1,7 +1,9 @@
-CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `lock_version` int(10) unsigned NOT NULL DEFAULT '0',
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
+-- 'qiita_stocker' というユーザー名のユーザーを '(YourPassword999)' というパスワードで作成
+-- データベース 'qiita_stocker' への権限を付与
+CREATE DATABASE IF NOT EXISTS qiita_stocker CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+GRANT ALL on qiita_stocker.* TO `qiita_stocker`@`%` identified BY '(YourPassword999)';
+
+-- 'qiita_stocker_test' というユーザー名のユーザーを '(YourPassword999)' というパスワードで作成
+-- データベース 'qiita_stocker_test' への権限を付与
+CREATE DATABASE IF NOT EXISTS qiita_stocker_test CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+GRANT ALL on qiita_stocker_test.* TO `qiita_stocker_test`@`%` identified BY '(YourPassword999)';
